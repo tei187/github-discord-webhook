30183ef48a6de7b0c5738c428cb1094e5c661717
tei187 <bonk.piotr@gmail.com>
2024-10-07 21:49:25 +0200

    Webhook and Payload changes (due to overrides).

--- src/Payloads/Abstract/PayloadAbstract.php
+++ src/Payloads/Abstract/PayloadAbstract.php
@@ -1,11 +1,16 @@
 <?php
 
-namespace tei187\GithubDiscordWebhook\Payloads\Abstract;
+namespace tei187\GitDisWebhook\Payloads\Abstract;
 
-use tei187\GithubDiscordWebhook\Handlers\ArrayHandler;
-use tei187\GithubDiscordWebhook\Interfaces\Payload as PayloadInterface;
+use tei187\GitDisWebhook\Handlers\ArrayHandler;
+use tei187\GitDisWebhook\Traits\UsesMagicGetter;
+use tei187\GitDisWebhook\Traits\PayloadUsesRepo;
+use tei187\GitDisWebhook\Interfaces\Payload as PayloadInterface;
 
 abstract class PayloadAbstract implements PayloadInterface {
+    use UsesMagicGetter,
+        PayloadUsesRepo;
+
     /**
      * @var string The raw JSON payload of the GitHub webhook request.
      */
@@ -23,16 +28,11 @@ abstract class PayloadAbstract implements PayloadInterface {
     protected ?string $subject;
 
     /**
-     * @var ?string *(optional)* The type of action that triggered the GitHub webhook. It is considered the action applied to the subject or event itself (e.g. `created`).
-     *              Whether it is used or not will depend on the abstraction classes and applicability.
+     * @var ?string *(optional)* The type of action that triggered the GitHub webhook. It is considered the action applied to the
+     *              subject or event itself (e.g. `created`). Whether it is used or not will depend on the abstraction classes and
+     *              applicability.
      */
     protected ?string $action;
-    
-    /**
-     * @var \stdClass The name of the GitHub repository that triggered the webhook. 
-     *                If the payload is parsed, it will have the following properties: `name`, `fullname`, `url`.
-     */
-    protected object $repo;
 
     /**
      * @var bool Indicates whether the payload is allowed to be processed in messaging.
@@ -51,9 +51,12 @@ abstract class PayloadAbstract implements PayloadInterface {
     /**
      * Checks if the payload is allowed to be processed based on the configuration.
      *
-     * This method filters the payload properties (event, subject, action) to create a dot-notation path, and then checks the corresponding value in the payloads.php configuration file. If the value is a boolean, it is used to set the $allowed property. Otherwise, $allowed is set to false.
+     * This method filters the payload properties (event, subject, action) to create a dot-notation path, and then checks the
+     * corresponding value in the payloads.php configuration file. If the value is a boolean, it is used to set the $allowed
+     * property. Otherwise, $allowed is set to false.
      *
      * @return $this The current instance of the PayloadAbstract class.
+     * @deprecated 1.1.0 Event validation is now handled by Webhook class, due to implementation of overrides configs.
      */
     public function checkAllowed(): self {
         $config = require(GHDWEBHOOK_ROOT . 'config/allowed.php');
@@ -77,45 +80,35 @@ abstract class PayloadAbstract implements PayloadInterface {
     /**
      * Gets a dotted path representation of the event, subject, and action properties.
      *
-     * This method constructs a dotted path string by filtering out any null or empty values from the event, subject, and action properties, and then joining the remaining values with periods.
+     * This method constructs a dotted path string by filtering out any null or empty values from the event, subject, and
+     * action properties, and then joining the remaining values with periods.
      *
      * @return string The dotted path representation of the event, subject, and action properties.
      */
-    public function getDottedPath(): string {
-        $path = array_filter(
-            [ $this->event, $this->subject, $this->action, ],
-            function($value) { return $value !== null && $value !== ''; }
-        );
+    final public function getDottedPath(): string {
+        $path = $this->getEventPath();
         return implode('.', array_map('trim', $path));
     }
 
-    // static classes
-    static public function makeRepo(object $decoded): \stdClass {
-        $repo = new \stdClass();
-        $repo->name     = (string) $decoded->repository->name;
-        $repo->fullname = (string) strtolower($decoded->repository->full_name);
-        $repo->url      = (string) $decoded->repository->html_url;
-        return $repo;
-    }
-    
-    // abstract classes
-    
     /**
-     * Parses the given payload string and returns the current instance of the PayloadAbstract class.
-     *
-     * This method is responsible for decoding the payload string and validating the resulting data structure.
-     * If the payload is valid, it sets appropriate properties on the current instance of the PayloadAbstract class.
-     *
-     * @param string $payload The payload string to be parsed.
-     * @return $this The current instance of the PayloadAbstract class.
+     * Gets an array representation of the event, subject, and action properties.
+     * This method returns an array representation of the event, subject, and action properties, filtering out any null or empty values.
+     * @return array An array representation of existing (not null) event, subject, and action properties.
+     * @see getDottedPath()
      */
-    abstract public function parse(string $payload): self;
-
-    // magic getter
-
-    public function __get($param) {
-        if(isset($this->$param)) {
-            return $this->$param;
-        }
+    final public function getEventPath(): array {
+        return ArrayHandler::filterNulls( [ $this->event, $this->subject, $this->action ] );
     }
+        
+    // abstract methods
+        /**
+         * Parses the given payload string and returns the current instance of the PayloadAbstract class.
+         *
+         * This method is responsible for decoding the payload string and validating the resulting data structure.
+         * If the payload is valid, it sets appropriate properties on the current instance of the PayloadAbstract class.
+         *
+         * @param string $payload The payload string to be parsed.
+         * @return $this The current instance of the PayloadAbstract class.
+         */
+        abstract public function parse(string $payload): self;
 }
--- src/Webhook.php
+++ src/Webhook.php
@@ -1,77 +1,86 @@
 <?php
 
-namespace tei187\GithubDiscordWebhook;
+namespace tei187\GitDisWebhook;
 
-use tei187\GithubDiscordWebhook\Handlers\ResponseHandler;
-use tei187\GithubDiscordWebhook\Interfaces\Payload as PayloadInterface;
+use tei187\GitDisWebhook\Enums\ConfigKeys;
+use tei187\GitDisWebhook\Handlers\ConfigHandler;
+use tei187\GitDisWebhook\Handlers\ArrayHandler;
+use tei187\GitDisWebhook\Handlers\OverridesHandler;
+use tei187\GitDisWebhook\Handlers\ResponseHandler;
+use tei187\GitDisWebhook\Traits\UsesMagicGetter;
+use tei187\GitDisWebhook\Interfaces\Webhook as WebhookInterface;
+use tei187\GitDisWebhook\Interfaces\Payload as PayloadInterface;
 
 /**
  * Represents a profile for a Discord webhook associated with a GitHub repository.
  * The `Profile` class encapsulates the URL, GitHub secret, and list of repositories associated with a webhook.
  * It also provides methods for validating the webhook request signature and setting the request object.
  */
-class Webhook implements \tei187\GithubDiscordWebhook\Interfaces\Webhook {
+class Webhook implements WebhookInterface {
+    use UsesMagicGetter;
+
+    protected bool $validated = false;
+    protected bool $allowed = false;
+
     /**
      * Constructs a new `Profile` instance with the given URL, secret, and repository list.
      *
-     * @param string|null            $url     The URL of Discord webhook associated with the profile.
-     * @param string|null            $secret  The GitHub secret associated with the profile.
-     * @param array|null             $repos   The list of repositories associated with the profile.
-     * @param PayloadInterface|null  $payload The request object associated with the profile.
-     * @param bool|null              $validated Whether the profile has been validated.
+     * @param string|null           $name      The name of the webhook profile.
+     * @param string|null           $url       The URL of Discord webhook associated with the profile.
+     * @param string|null           $secret    The GitHub secret associated with the profile.
+     * @param array|null            $repos     The list of repositories associated with the profile.
+     * @param PayloadInterface|null $payload   The payload object used with the profile.
+     * @param array|null            $overrides Array of overrides to allowed payloads and messages.
+     *                                         If `null`, the profile will use configured overrides by default.
+     * @param bool                  $validated Whether the profile has been validated.
+     * @param bool|null             $allowed   Whether the profile is allowed to be processed in messaging. 
+     *                                         It can be `null` at some point, if event path is not found in webhook's overrides,
+     *                                         at which point it will check the default configuration.
      */
     final function __construct(
-        private ?string $name = null,
-        private ?string $url = null,
-        private ?string $secret = null,
-        private ?array  $repos = [],
-        private ?PayloadInterface $payload = null,
-        private ?bool $validated = false
+        protected ?string $name = null,
+        protected ?string $url = null,
+        protected ?string $secret = null,
+        protected ?array  $repos = [],
+        protected ?PayloadInterface $payload = null,
+        protected ?array  $overrides = null,
     ) {
-        if(is_array($this->repos)) {
-            $this->repos = array_map('strtolower', $this->repos);
-        } else {
-            $this->repos = [];
-        }
 
+        // set payload
+        $this->setPayload($payload);
+
+        // set repos
+        $this->repos = is_array($this->repos) 
+            ? array_map('strtolower', $this->repos) 
+            : [];
+
+        // validate profile
         $this->validated = $this->validateProfile() ?: false;
 
-        if($this->request !== null) {
-            $this->setPayload($payload);
-        }
+        // set overrides
+        $this->overrides = is_array($overrides) && !empty($overrides)
+            ? $overrides
+            : OverridesHandler::load($this);
+
+        // validate event allowed
+        $this->allowed = $this->validateEvent();
     }
 
     /**
-     * Sets the request object associated with the profile.
+     * Sets the payload object associated with the profile.
      * Enforces signature validation of the payload instance. If the signature is invalid, an error response is sent back to repository.
      *
-     * @param \tei187\GithubDiscordWebhook\Interfaces\Payload $request The request object to set.
+     * @param PayloadInterface $payload The request object to set.
+     * @return $this The current instance of the Webhook class profile.
      */
-    final public function setPayload(PayloadInterface $payload) {
+    final public function setPayload(PayloadInterface $payload): self {
         $this->payload = $payload;
 
         if($this->validateSignature() === false) {
-            ResponseHandler::send("Invalid payload signature", "error", 401);
+            ResponseHandler::send("Invalid payload signature.", "error", 401);
         }
-    }
 
-    /**
-     * Retrieves the value of the specified property from the `Profile` instance.
-     *
-     * @param string $param The name of the property to retrieve.
-     * @return mixed The value of the specified property, or throws an `Exception` if the property is invalid.
-     */
-    function __get($param) {
-        if(isset($this->$param)) {
-            return match ($param) {
-                'url'       => $this->url,
-                'secret'    => $this->secret,
-                'repos'     => $this->repos,
-                'payload'   => $this->payload,
-                'validated' => $this->validated,
-                default     => null,
-            };
-        }
+        return $this;
     }
 
     /**
@@ -80,12 +89,11 @@ class Webhook implements \tei187\GithubDiscordWebhook\Interfaces\Webhook {
      * @return true|void True if the profile is valid, otherwise ReponseHandler void.
      */
     final public function validateProfile(): bool {
-        if ($this->url === null) {
-            ResponseHandler::send("Webhook not configured properly. Discord URL is required.", "error", 500);
-        }
-        if ($this->secret === null) {
-            ResponseHandler::send("Webhook not configured properly. Secret is required.", "error", 500);
-        }
+        match(null) {
+            $this->url    => ResponseHandler::send("Webhook not configured properly. Discord URL is required.", "error", 500),
+            $this->secret => ResponseHandler::send("Webhook not configured properly. Secret is required.", "error", 500),
+            default => null
+        };
         return true;
     }
 
@@ -93,19 +101,56 @@ class Webhook implements \tei187\GithubDiscordWebhook\Interfaces\Webhook {
      * Validates the signature of the incoming webhook request by comparing the
      * calculated HMAC signature with the signature provided in the request headers.
      *
-     * @throws \Exception If repo name is not found in the repositories list for this profile.
      * @return bool|void True if the signature is valid, false otherwise.
      */
     final public function validateSignature(): bool {
+        // check if repo is allowed to be handled by webhook profile
         if(!empty($this->repos)) {
-            if(!in_array($this->request->repo->fullname, $this->repos)) {
-                ResponseHandler::send("This webhook is not configured to handle this repository.", "error", 422);
+            if(!in_array($this->payload->repo->fullname, $this->repos)) {
+                ResponseHandler::send("This webhook is not configured to handle calls from this repository.", "error", 406);
             }
         }
 
-        return hash_equals(
-            'sha1=' . hash_hmac('sha1', $this->request->plain, $this->secret),
-            $_SERVER['HTTP_X_HUB_SIGNATURE']
-        );
+        // Get the GitHub signature from the headers
+        $githubSignature = $_SERVER['HTTP_X_HUB_SIGNATURE'] ?? '';
+
+        // Calculate our signature
+        $calculatedSignature = 'sha1=' . hash_hmac('sha1', $this->payload->plain, $this->secret);
+
+        // Compare signatures
+        return hash_equals($calculatedSignature, $githubSignature);
+    }
+
+    /**
+     * Validates the event payload to ensure it is allowed to be handled by the webhook profile.
+     * If the payload event path is not allowed, an error response is sent back to the repository.
+     *
+     * @return bool|void True if the event payload is allowed, false otherwise.
+     */
+    final public function validateEvent(): bool {
+        $path = $this->payload->getDottedPath();
+
+        // check if override is set
+        $allowed = is_array($this->overrides) && key_exists('allowed', $this->overrides)
+            ? ArrayHandler::getValueByDotNotation($this->overrides['allowed'], $path)
+            : null;
+
+        // check allowed state
+        if(is_bool($allowed) && !$allowed) {
+            ResponseHandler::send("This webhook does not allow handling of this payload or event path. Check configuration.", "error", 406);
+            return false;
+        } elseif(is_null($allowed)) {
+            // if path is not found in overrides, check if it's allowed by default
+            $config  = ConfigHandler::load(ConfigKeys::ALLOWED);
+            $allowed = ArrayHandler::getValueByDotNotation($config, $path);
+
+            // if path is still not found in config, then assume it's not allowed
+            if($allowed === null) {
+                ResponseHandler::send("This webhook does is not configured to handle this payload or event. Check configuration.", "error", 406);
+                return false;
+            }
+        }
+
+        return $allowed;
     }
 }
