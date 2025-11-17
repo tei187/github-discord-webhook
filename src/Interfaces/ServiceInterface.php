9bded52575b29e9f02b6decd1c8a43d12f0202e1
tei187 <bonk.piotr@gmail.com>
2025-11-17 14:51:15 +0100

    Added default payload and message classes attribution for services without specific factories

--- src/Interfaces/ServiceInterface.php
+++ src/Interfaces/ServiceInterface.php
@@ -15,34 +15,6 @@ use tei187\GitDisWebhook\Interfaces\MessageFactoryInterface;
  */
 interface ServiceInterface {
     /**
-    * Validates the provided payload.
-    *
-    * This method checks if the given payload is valid according to the service's or platform's specific rules.
-    * If you really don't want to validate anything, just return true (bad idea, seen worse).
-    *
-    * @param string $payload The payload to validate.
-    * @return bool True if the payload is valid, false otherwise.
-    */
-    public function validatePayload(string $payload): bool;
-    
-    /**
-     * Validates the event based on the compiled allowed events from configuration and overrides.
-     * Requires that the service name, webhook, and payload are set.
-     *
-     * @return bool True if the event is valid, false otherwise.
-     */
-    public function validateEvent(): bool;
-
-    /**
-     * Sets the payload for the service.
-     *
-     * @param PayloadInterface|string|null $payload The payload to set. Directly assigns if PayloadInterface, or instantiates if string.
-     * @param string|null $payloadClass The class name to instantiate if payload is a string. Required if `$payload` is string, otherwise irrelevant.
-     * @return self
-     */
-    public function setPayload(PayloadInterface|string|null $payload, ?string $payloadClass = null): self;
-
-    /**
     * Sets the webhook for the service.
     *
     * @param WebhookInterface|null $webhook The webhook to set.
@@ -57,6 +29,15 @@ interface ServiceInterface {
      * @return self
      */
     public function setConfig(Config $config): self;
+
+    /**
+     * Sets the payload for the service.
+     *
+     * @param PayloadInterface|string|null $payload The payload to set. Directly assigns if PayloadInterface, or instantiates if string.
+     * @param string|null $payloadClass The class name to instantiate if payload is a string. Required if `$payload` is string, otherwise irrelevant.
+     * @return self
+     */
+    public function setPayload(PayloadInterface|string|null $payload, ?string $payloadClass = null): self;
 
     /**
      * Sets the payload factory for the service.
@@ -78,9 +59,10 @@ interface ServiceInterface {
      * Sets the message instance.
      *
      * @param MessageInterface|null $message The message instance to set.
+     * @param string|null $messageClass The message class to instantiate if message is null. Required if `$message` is null, otherwise irrelevant.
      * @return $this
      */
-    public function setMessage(?MessageInterface $message = null): self;
+    public function setMessage(?MessageInterface $message = null, ?string $messageClass = null): self;
 
     /**
      * Sets the message factory instance.
@@ -97,6 +79,25 @@ interface ServiceInterface {
      * @return self
      */
     public function setMessageThroughFactory(string $event): self;
+
+    /**
+    * Validates the provided payload.
+    *
+    * This method checks if the given payload is valid according to the service's or platform's specific rules.
+    * If you really don't want to validate anything, just return true (bad idea, seen worse).
+    *
+    * @param string $payload The payload to validate.
+    * @return bool True if the payload is valid, false otherwise.
+    */
+    public function validatePayload(string $payload): bool;
+    
+    /**
+     * Validates the event based on the compiled allowed events from configuration and overrides.
+     * Requires that the service name, webhook, and payload are set.
+     *
+     * @return bool True if the event is valid, false otherwise.
+     */
+    public function validateEvent(): bool;
 
     /**
      * Gets the repository name from the payload in config.
--- src/Services/Abstract/ServiceAbstract.php
+++ src/Services/Abstract/ServiceAbstract.php
@@ -63,6 +63,18 @@ abstract class ServiceAbstract implements ServiceInterface {
          */
         protected ?PayloadFactoryInterface $payloadFactory;
 
+
+        /**
+         * Class name of the service-specific payload.
+         * 
+         * Most likely, it will be tied with the service, hence by default it is set to null and should be defined in child classes.
+         * For more complex services, it may be better to use the payload factory instead of direct class instantiation, unless you have
+         * a specific one-fit-all payload class.
+         * 
+         * @var string|null
+         */
+        protected ?string $payloadClass = null;
+
         /**
          * Instance of the payload.
          * @var PayloadInterface|null
@@ -85,6 +97,17 @@ abstract class ServiceAbstract implements ServiceInterface {
          * @var MessageFactory|null
         */
         protected ?MessageFactoryInterface $messageFactory;
+
+        /**
+         * Class name of the service-specific payload.
+         * 
+         * Most likely, it will be tied with the service, hence by default it is set to null and should be defined in child classes.
+         * For more complex services, it may be better to use the message factory instead of direct class instantiation, unless you have
+         * a specific one-fit-all message class.
+         * 
+         * @var string|null
+         */
+        protected ?string $messageClass = null;
         
         /**
          * Instance of the message handler.
@@ -101,32 +124,34 @@ abstract class ServiceAbstract implements ServiceInterface {
      * @param MessageInterface|null $message The message factory for the service.
      */
     public function __construct(?Config $config = null, ?PayloadInterface $payload = null, ?WebhookInterface $webhook = null, ?MessageInterface $message = null) {
-        // set config
+        // set config through injection or create new instance if not provided
         $config = $config ?? new Config();
         $this->setConfig($config);
 
-        // set webhook if provided
-        if($webhook !== null) {
-            $this->setWebhook($webhook);
-        }
+        // set webhook, if provided
+        if($webhook !== null) { $this->setWebhook($webhook); }
 
-        // set payload if provided, else create through factory if class is defined
+        // set payload ...
         if($payload !== null) {
+            // ... directly, if provided
             $this->setPayload($payload);
         } elseif ($payload === null && $this->payloadFactoryClass !== null) {
+            // ... through factory, if class is defined
             $this->setPayloadFactory()->setPayloadThroughFactory(file_get_contents('php://input'));
         }
 
-        // set message if provided, else create through factory if class is defined
+        // set message ...
         if($message !== null) {
+            // ... directly, if provided
             $this->setMessage($message);
         } elseif ($message === null && $this->messageFactoryClass !== null && $this->webhook !== null) {
-            $this->setMessageFactory()->setMessageThroughFactory($this->payload?->event ?? '', $this->webhook);
+            // ... through factory, if class is defined
+            $this->setMessageFactory()->setMessageThroughFactory($this->payload?->event ?? '');
         }
     }
 
     final public function setWebhook(?WebhookInterface $webhook): self {
-        $this->webhook = $webhook;
+        $this->webhook = $webhook; 
         return $this;
     }
     
@@ -149,10 +174,16 @@ abstract class ServiceAbstract implements ServiceInterface {
             $check = $this->validatePayload($payload);
 
             if($payload instanceof PayloadInterface) {
+                // assign provided payload instance
                 $this->payload = $payload;
             } elseif (is_string($payload) && $payloadClass !== null && class_exists($payloadClass)) {
+                // instantiate specified payload class
                 $this->payload = new $payloadClass($payload);
+            } elseif (is_string($payload) && $this->payloadClass !== null && class_exists($this->payloadClass)) {
+                // instantiate default payload class
+                $this->payload = new $this->payloadClass($payload);
             } else {
+                // reset payload to null
                 $this->payload = null;
             }
 
@@ -168,18 +199,12 @@ abstract class ServiceAbstract implements ServiceInterface {
          * @throws \InvalidArgumentException If the provided payload factory class does not exist.
          */
         final public function setPayloadFactory(?string $payloadFactoryClass = null): self {
+            // use provided class or default one
             $payloadFactoryClass = $payloadFactoryClass ?? $this->payloadFactoryClass;
-
-            // no class provided
-            if($payloadFactoryClass === null) {
-                throw new \InvalidArgumentException("No payload factory class provided.");
-            }
-            
-            // class does not exist
-            if(!class_exists($payloadFactoryClass)) {
-                throw new \InvalidArgumentException("Payload factory class does not exist: {$payloadFactoryClass}");
-            }
-
+            // throw: no class provided
+            if($payloadFactoryClass === null) { throw new \InvalidArgumentException("No payload factory class provided."); }            
+            // throw: class does not exist
+            if(!class_exists($payloadFactoryClass)) { throw new \InvalidArgumentException("Payload factory class does not exist: {$payloadFactoryClass}"); }
             // instantiate factory
             $this->payloadFactory = new $payloadFactoryClass($this->config);
 
@@ -190,11 +215,8 @@ abstract class ServiceAbstract implements ServiceInterface {
          * @throws \InvalidArgumentException If the payload factory is not set.
          */
         final public function setPayloadThroughFactory(string $payload): self {
-            // ensure payload factory is set
-            if(!isset($this->payloadFactory)) {
-                throw new \InvalidArgumentException("Payload factory is not set.");
-            }
-
+            // throw: payload factory is not set
+            if(!isset($this->payloadFactory)) { throw new \InvalidArgumentException("Payload factory is not set."); }
             // create payload through factory
             $this->payload = $this->payloadFactory->createPayload($payload);
 
@@ -203,12 +225,26 @@ abstract class ServiceAbstract implements ServiceInterface {
 
     // MESSAGE MANAGEMENT
 
-        final public function setMessage(?MessageInterface $message = null): self {
-            $this->message = $message;
+        final public function setMessage(MessageInterface|null $message = null, ?string $messageClass = null): self {
+            if($message instanceof MessageInterface) {
+                // assign provided message instance
+                $this->message = $message;
+            } elseif ($message === null && $messageClass !== null && class_exists($messageClass)) {
+                // instantiate specified message class
+                $this->message = new $messageClass();
+            } elseif ($message === null && $this->messageFactoryClass !== null && class_exists($this->messageFactoryClass)) {
+                // instantiate message through message factory class
+                $this->message = new $this->messageFactoryClass();
+            } else {
+                // reset message to null
+                $this->message = null;
+            }
             return $this;
         }
 
         final public function setMessageFactory(?MessageFactoryInterface $messageFactory = null): self {
+            // assign provided factory ($messageFactory) or
+            // instantiate specified factory with current config, service name and webhook
             $this->messageFactory = $messageFactory ?? new MessageFactory($this->config, $this->name, $this->webhook);
             return $this;
         }
@@ -217,16 +253,10 @@ abstract class ServiceAbstract implements ServiceInterface {
          * @throws \InvalidArgumentException If the message factory or webhook is not set.
          */
         final public function setMessageThroughFactory(string $event): self {
-            // ensure message factory is set
-            if(!isset($this->messageFactory)) {
-                throw new \InvalidArgumentException("Message factory is not set.");
-            }
-
-            // ensure webhook is set
-            if(!isset($this->webhook)) {
-                throw new \InvalidArgumentException("Webhook is not set.");
-            }
-
+            // throw: message factory is not set
+            if(!isset($this->messageFactory)) { throw new \InvalidArgumentException("Message factory is not set."); }
+            // throw: webhook is not set
+            if(!isset($this->webhook)) { throw new \InvalidArgumentException("Webhook is not set."); }
             // create message through factory
             $this->message = $this->messageFactory->createMessage($event, $this->payload, $this->webhook);
 
@@ -238,11 +268,12 @@ abstract class ServiceAbstract implements ServiceInterface {
     abstract public function getRepositoryBranch(): ?string;
 
     public function validateEvent(): bool {
+        // merge allowed events from config and webhook profile overrides
         $compiledEvents = ArrayHandler::array_merge_deep_overwrite(
             $this->config->allowed[$this->name] ?? [],
             $this->config->profiles[$this->webhook->name]['overrides']['allowed'] ?? []
         );
-
+        // check if event path exists in compiled allowed events
         return ArrayHandler::getValueByDotNotation( $compiledEvents, $this->payload->getDottedEventPath() ) ?? true;
     }
 }
